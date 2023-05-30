381e915a3a8d54f4cce2c0df2af240304186eb1e
KANTNOLI <zahargysev02@gmail.com>
2023-05-30 17:38:15 +0300

    Ready

--- PizzaWork-4.5-LAST/Source.cpp
+++ PizzaWork-4.5-LAST/Source.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -10,20 +11,58 @@ using namespace std;
 //параметром.С помощью этой функции на основе данного массива A размера K и
 //целых чисел M и N сформировать матрицу B размера M x N
 
-void ArrayToMatrCol() {
+void ArrayToMatrCol(int* vector, int sizeVector, int** matrix, int matSizeX, int matSizeY) {
+	int count = 0;
+	for (int i = 0; i < matSizeY; i++)
+	{
+		for (int j = 0; j < matSizeX; j++) {
+			if (count < sizeVector) {
+				matrix[i][j] = vector[count]; 
+				count++;
+			}
+			else {
+				matrix[i][j] = 0;
+			}
+		}
+	}
 
 }
 
 int main() {
-	int k = 0;
-	int M, X;
+	setlocale(LC_ALL, "RUS");
+	int sizeVector;
+	int matSizeX, matSizeY;
+	int input;
+	string msg = "Матрица: \n";
+	cout << "Введите размер матрицы X - Y: ";
+	cin >> matSizeX >> matSizeY; 
 
-	int* A = new int[k];
-
-	int** B = new int* [M];
-	for (int i = 0; i < M; i++) {
-		B[i] = new int[X];
+	int** matrix = new int* [matSizeY];
+	for (int i = 0; i < matSizeY; i++) {
+		matrix[i] = new int[matSizeX];
 	}
+
+	cout << "Введите размер массива для заполнения матрицы: ";
+	cin >> sizeVector ;
+
+	int* vector = new int[sizeVector];
+
+	cout << "Введите элементы массива: ";
+	for (int i = 0; i < sizeVector; i++) {
+		cin >> vector[i];
+	}
+
+	ArrayToMatrCol(vector, sizeVector, matrix, matSizeX, matSizeY); 
+	 
+	for (int i = 0; i < matSizeY; i++) 
+	{  
+		for (int j = 0; j < matSizeX; j++) {  
+			msg += to_string(matrix[i][j]) + "  ";  
+		}
+		msg += "\n"; 
+	}
+
+	cout << msg << endl; 
 
 	return 0;
 }
